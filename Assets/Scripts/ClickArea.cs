627e9b4c5b4218b10e5991ebbef6f730801ecbb7
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-10-20 10:02:58 +0200

    Create ClickArea
    Created a ClickArea in stead of a button

---
+++ Assets/Scripts/ClickArea.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickArea : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rect bounds = new Rect(0, 0, Screen.width / 2, Screen.height);
+        if (Input.GetMouseButtonDown(0) && bounds.Contains(Input.mousePosition))
+        {
+            Debug.Log("CLICKED!!");
+        }
+    }
+}
