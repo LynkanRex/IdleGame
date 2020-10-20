9cffe08d8cd62fa0eb474467ea99fc23a76a2971
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-10-20 14:47:32 +0200

    rename GoldPress.cs into Product.cs
    added generic variables and values

---
+++ Assets/Scripts/Product.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Product : MonoBehaviour
+{
+    [SerializeField] string productName;
+    [SerializeField] int productCost = 100;
+    [SerializeField] int productProductionTime = 1;
+    [SerializeField] int productProductionAmount = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
