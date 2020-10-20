037001eaeceb6bd1b08013c748a441f28fe3afd6
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-10-20 09:54:38 +0200

    Add GameCanvas, GoldText, Update GenerateGold.cs
    Added variables and references to GenerateGold
    
    Created GameCanvas and GoldText

--- Assets/Scripts/GenerateGold.cs
+++ Assets/Scripts/GenerateGold.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GenerateGold : MonoBehaviour
 {
+    public int goldAmount;
+    public Text goldText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        goldAmount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        goldText.text = "Gold: " + goldAmount;
     }
 }
