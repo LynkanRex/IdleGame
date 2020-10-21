a03ed20d5d4dde550af4876e187835f740496c10
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-10-21 13:25:37 +0200

    Fixes , GoldPress.cs
    Fixed some breaks from commit revertion
    
    Added GoldPress.cs from Marc's project

--- Assets/Scripts/Product.cs
+++ Assets/Scripts/Product.cs
@@ -5,6 +5,8 @@ using UnityEngine.UI;
 public class Product : MonoBehaviour
 {
 
+    public PurchasableProduct purchasableProduct;
+    
     //public PurchasableProduct[] purchasableProduct;
     public GameObject buttonCanvas;
 
@@ -30,80 +32,48 @@ public class Product : MonoBehaviour
         //     var myView = myGameObject.GetComponent<ConfigurableProductView>();
         //     myView.productButtonText.text = purchasableProduct[i].productName;
         // }
-<<<<<<< HEAD
-=======
-        get => productOwnedAmount;
-        set
-        {
-            productOwnedAmount = value;
-            UpdateProductTextLabel();
-        }
+        //UpdateProductTextLabel();
+        //}
     }
-    
-    private Component _productComponent;
-    public Component ProductComponent
-    {
-        get
-        {
-            return _productComponent;
-        }
-        private set
-        {
-            _productComponent = value;
-        }
-    }
-    
-    
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        productNameText.text = productName + ": " + productOwnedAmount;
-        StartCoroutine(nameof(ProductGenerateGold));
-        ProductComponent = GetComponentInParent<Gold>();
-=======
->>>>>>> parent of 6a7922f... Update Product.cs
-    }
-    
+
     // Update is called once per frame
     void Update()
     {
-        CanAffordProductCheck();
+        //CanAffordProductCheck();
     }
 
     void CanAffordProductCheck()
     {
-         var gameCanvas = GameObject.Find("GameCanvas");
-         var gold = gameCanvas.GetComponentInParent<Gold>();
-         if (gold.GoldAmount < purchasableProduct[0].productCost)
-         {
-             purchasableProduct[0].productView.productButtonText.color = Color.red;
-         }
-         else
-         {
-             purchasableProduct[0].productButtonText.color = Color.black;
-         }
+         // var gameCanvas = GameObject.Find("GameCanvas");
+         // var gold = gameCanvas.GetComponentInParent<Gold>();
+         // if (gold.GoldAmount < purchasableProduct[0].productCost)
+         // {
+         //     purchasableProduct[0].productView.productButtonText.color = Color.red;
+         // }
+         // else
+         // {
+         //     purchasableProduct[0].productButtonText.color = Color.black;
+         // }
     }
 
-    void UpdateProductTextLabel()
-    {
+    //void UpdateProductTextLabel()
+    //{
         //purchasableProduct[0].productNameText.text = purchasableProduct[0].productName + ": " + purchasableProduct[0].ProductOwnedAmount;
-    }
+    //}
 
-    public void BuyProduct()
-    {
-         var gold = GetComponentInParent<Gold>();
-         if (gold.GoldAmount > purchasableProduct[0].productCost)
-         {
-             gold.GoldAmount -= purchasableProduct[0].productCost;
-             purchasableProduct[0].ProductOwnedAmount += 1;
-         }
-         else
-         {
-             Debug.Log("Not enough gold!");
-         }
-    }
+    //public void BuyProduct()
+    //{
+    //     var gold = GetComponentInParent<Gold>();
+     //    if (gold.GoldAmount > purchasableProduct[0].productCost)
+    //     {
+    //         gold.GoldAmount -= purchasableProduct[0].productCost;
+    //         purchasableProduct[0].ProductOwnedAmount += 1;
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("Not enough gold!");
+    //     }
+    //}
 
     // IEnumerator ProductGenerateGold()
     // {
---
+++ Assets/Scripts/Products/GoldPress.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldPress : MonoBehaviour {
+    public int productionAmount = 1;
+    public int costs = 100;
+    public float productionTime = 1f;
+    public Text goldAmountText;
+    float elapsedTime;
+
+    public int GoldPressAmount {
+        get => PlayerPrefs.GetInt("GoldPress", 0);
+        set {
+            PlayerPrefs.SetInt("GoldPress", value);
+            UpdateGoldPressAmountLabel();
+        }
+    }
+
+    void UpdateGoldPressAmountLabel() {
+        this.goldAmountText.text = this.GoldPressAmount.ToString("0 GoldPresses");
+    }
+
+    void Start() {
+        UpdateGoldPressAmountLabel();
+    }
+	
+    void Update() {
+        this.elapsedTime += Time.deltaTime;
+        if (this.elapsedTime >= this.productionTime) {
+            ProduceGold();
+            this.elapsedTime -= this.productionTime; // DO NOT SET TO ZERO HERE
+        }
+    }
+    // something costs 100ct, and I get 40ct per day:
+    // IN CASE WE SET IT TO ZERO:
+    // 40ct 80ct [120ct (Buy for 100ct) 0ct] 40ct 80ct // In 5 Days, I can buy something for 100ct once, and I have 80ct
+    // IN CASE WE DECREASE IT BY THE COSTS:
+    // 40ct 80ct [120ct (Buy for 100ct) 20ct] 60ct [100ct (Buy for 100ct) 0ct] // In 5 Days, I can buy something for 100ct twice
+
+    void ProduceGold() {
+        var gold = FindObjectOfType<Gold>();
+        gold.GoldAmount += this.productionAmount * this.GoldPressAmount;
+    }
+
+    public void BuyGoldPress() {
+        var gold = FindObjectOfType<Gold>();
+        if (gold.GoldAmount >= this.costs) {
+            gold.GoldAmount -= this.costs;
+            this.GoldPressAmount += 1;
+        }
+    }
+}
