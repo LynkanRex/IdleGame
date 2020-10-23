92f0adf3168df39c49706dac1eec3c8905bc4bd3
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-10-23 18:38:03 +0200

    Update UI, Add Enemy Name and health Labels

--- Assets/Scripts/BattleSceneScripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/BattleSceneScripts/Enemy/EnemySpawner.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,17 +9,33 @@ public class EnemySpawner : MonoBehaviour
 {
     public EnemyData EnemyData;
 
+    public Text enemyNameLabel;
+    public Text enemyHealthLabel;
+    
     public void SetUp(EnemyData enemyData) {
         this.EnemyData = enemyData;
         this.gameObject.name = enemyData.name;
+        this.enemyNameLabel.text = enemyData.name;
+        //this.enemyHealthLabel.text = enemyData.health.ToString($"0 Hp" + " / " + "0 Hp");
     }
 	
     public int EnemiesKilledAmount {
         get => PlayerPrefs.GetInt(this.EnemyData.name, 0);
         set {
             PlayerPrefs.SetInt(this.EnemyData.name, value);
+            UpdateEnemyNameLabel();
         }
     }
+
+    private void Start()
+    {
+        UpdateEnemyNameLabel();
+    }
+
+    void UpdateEnemyNameLabel() {
+        this.enemyNameLabel.text = this.EnemyData.name;
+    }
+
     
     void Update() {
             
