d471bd486f120003ba060d19cabe9e3c8158650f
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-10-23 11:24:50 +0200

    Split Attack logic into Actions
    Updated Enemy and Hero objects with Health, TakeDamage functions
    
    Split Attack into Actions.cs component

---
+++ Assets/Scripts/BattleSceneScripts/Actions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actions : MonoBehaviour
+{
+
+    private int damage = 5;
+    private float currentTime;
+    [SerializeField] private float timer = 0.6f;
+
+    [SerializeField] GameObject target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+        if (currentTime >= timer)
+        {
+            Attack(target);
+            currentTime = 0f;
+        }
+    }
+    
+    public void Attack(GameObject target)
+    {
+        target.SendMessage("TakeDamage", damage);
+        Debug.Log("Attacking: " + target + "!");
+    }
+}
---
+++ Assets/Scripts/BattleSceneScripts/Hero.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Hero : MonoBehaviour
+{
+
+    //private float currentTime;
+    //private float timer = 0.6f;
+
+    private Actions actions;
+    //private GameObject target;
+
+    public int lifePoints = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //target = GameObject.Find("Enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // currentTime += Time.deltaTime;
+        // if (currentTime >= timer)
+        // {
+        //     actions.Attack(target);
+        //     currentTime = 0f;
+        // }
+    }
+
+
+    public void TakeDamage(int damage)
+    {
+        this.lifePoints -= damage;
+    }
+}
