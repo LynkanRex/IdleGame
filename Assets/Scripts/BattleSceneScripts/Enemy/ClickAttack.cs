fa765f8637aca9da0a5411923d5616a8bed07993
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-05 10:09:43 +0100

    Enemy Clickable
    Takes 3 damage when enemys graphic is clicked

---
+++ Assets/Scripts/BattleSceneScripts/Enemy/ClickAttack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAttack : MonoBehaviour
+{
+
+    [SerializeField] private float clickDamage;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoClickAttack()
+    {
+        Debug.Log("Clicking enemy");
+        var enemy = GetComponentInParent<Unit>();
+        enemy.TakeDamage(clickDamage);
+    }
+}
---
+++ Assets/Scripts/BattleSceneScripts/Enemy/EmptyGraphic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.UI;
+
+/// <summary>
+/// This component should be used, whenever you need elements to be hit by a Graphic-Raycaster, without
+/// producing any necessary drawcalls. Most common usage: on Button gameObjects to add a rect collider.
+/// </summary>
+public class EmptyGraphic : Graphic {
+#if !UNITY_5_0 && !UNITY_5_1
+    protected override void OnPopulateMesh(VertexHelper vh) {
+        vh.Clear();
+    }
+#else
+	protected override void OnFillVBO (List<UIVertex> vbo) {
+		vbo.Clear();
+	}
+#endif
+}
