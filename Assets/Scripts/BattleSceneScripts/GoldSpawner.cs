77593b7d340d997a14a360bc833a85be316ff381
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-05 17:28:57 +0100

    Gold now displays and updates current gold

--- Assets/Scripts/BattleSceneScripts/GoldSpawner.cs
+++ Assets/Scripts/BattleSceneScripts/GoldSpawner.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GoldSpawner : MonoBehaviour
 {
+    private Text goldText;
+    private Hero hero;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        hero = FindObjectOfType<Hero>();
+        goldText = GetComponent<Text>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.goldText.text = hero.goldAmount.ToString("0 Gold");
     }
 }
